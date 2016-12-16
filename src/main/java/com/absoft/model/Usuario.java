adc0e7ec794b0c1e5e2266b0ea1caabd8e6224ea
D.Arantes <D.Arantes@TI-002.INCOPOSTES.COM>
2016-12-16 08:30:19 -0200

    Adicionado unique no usuario

--- src/main/java/com/absoft/model/Usuario.java
+++ src/main/java/com/absoft/model/Usuario.java
@@ -7,20 +7,24 @@ import org.bson.types.ObjectId;
 import org.mongodb.morphia.annotations.Embedded;
 import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.IndexOptions;
+import org.mongodb.morphia.annotations.Indexed;
 
 /**
  * Usuários do sistema
- * @author Diego Arantes
- * Data alteração 15/12/2016
+ *
+ * @author Diego Arantes Data alteração 15/12/2016
  */
 @Entity(value = "usuarios", noClassnameStored = true)
 @Data
-public class Usuario implements BasicEntity{
+public class Usuario implements BasicEntity {
 
     @Id
     private ObjectId id;
 
     private String nome;
+
+    @Indexed(options = @IndexOptions(unique = true))
     private String usuario;
     private String senha;
     private String email;
@@ -28,7 +32,7 @@ public class Usuario implements BasicEntity{
     private boolean online;
     private String ipUltimoLogon;
     private Date horaUltimoLogon;
-    
+
     @Embedded
     private List<Permissao> permissoes;
 }
