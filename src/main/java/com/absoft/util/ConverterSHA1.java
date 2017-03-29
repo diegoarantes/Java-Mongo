6f8a6824b4b88431b516887b65c5300baa764e35
D.Arantes <D.Arantes@TI-002.INCOPOSTES.COM>
2017-03-29 15:25:00 -0300

    Melhoria conversor

--- src/main/java/com/absoft/util/ConverterSHA1.java
+++ src/main/java/com/absoft/util/ConverterSHA1.java
@@ -4,39 +4,18 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
-import javax.faces.convert.Converter;
-import javax.faces.convert.FacesConverter;
 
 /**
- *
  * @author Diego Arantes
- *
- * Algoritmo para converter strings em SHA1. Muito utilizado em criptografia de
- * senhas
+ * @description Algoritmo para converter Strings para SHA1.
  */
-@FacesConverter(value = "converterSHA1")
-public class ConverterSHA1 implements Converter {
-
-    @Override
-    public Object getAsObject(FacesContext context, UIComponent component, String value) {
-        return value.isEmpty() ? "" : cipher(value);//Se o valor for vazio retorna "", senão converte
-    }
-
-    @Override
-    public String getAsString(FacesContext context, UIComponent component, Object value) {
-        return value.toString();
-    }
+public class ConverterSHA1 {
 
     public static String cipher(String value) {
-        String algorithm = "SHA1";
-       // value = "L0uKur4" + value + "9/8*X5.S.8-";
-
         byte[] buffer = value.getBytes();
-        MessageDigest md;
+
         try {
-            md = MessageDigest.getInstance(algorithm);
+            MessageDigest md = MessageDigest.getInstance("SHA1");
             md.update(buffer);
             byte[] digest = md.digest();
             String hexValue = "";
@@ -53,5 +32,4 @@ public class ConverterSHA1 implements Converter {
             return null;
         }
     }
-
 }
