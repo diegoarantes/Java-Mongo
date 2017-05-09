4aa3ab5ab26ba6aeb76219fe03a2991eec3f3903
D.Arantes <D.Arantes@TI-002.INCOPOSTES.COM>
2017-05-09 16:11:49 -0300

    Configurações Iniciais Banco de dados

---
+++ db-inicial.js
@@ -0,0 +1,8 @@
+db.usuarios.insert({
+    "nome" : "Diego Arantes",
+    "email" : "dyegoh16@gmail.com",
+    "usuario" : "admin",
+    "senha" : "d033e22ae348aeb5660fc2140aec35850c4da997",
+    "ativo" : true,
+    "online" : false,
+});
